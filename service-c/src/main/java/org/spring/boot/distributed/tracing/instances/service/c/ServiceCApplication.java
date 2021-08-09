9ee29eac0ba8d53375fedbb3026a54b2278677ba
Alexander A. Kropotin <ololx@icloud.com>
2021-08-09 20:47:00 +0500

    feature(service-c/*) init new service for the consuming messages

---
+++ service-c/src/main/java/org/spring/boot/distributed/tracing/instances/service/c/ServiceCApplication.java
@@ -0,0 +1,18 @@
+package org.spring.boot.distributed.tracing.instances.service.c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @project service-c
+ * @created 2021-08-09 20:43
+ * <p>
+ * @author Alexander A. Kropotin
+ */
+@SpringBootApplication
+public class ServiceCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCApplication.class, args);
+    }
+}
